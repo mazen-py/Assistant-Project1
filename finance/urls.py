58383277ad5d38ad2bbb5391bd25e6d5d63131ac
Mazen Elsayed <mazenkue@gmail.com>
2026-04-18 19:15:35 +0200

    2
    Removed comment about home page view.

--- finance/urls.py
+++ finance/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .views import finance_home
 
 urlpatterns = [
-    # This just points to your home page view. No includes!
     path('', finance_home, name='finance_home'),
 ]
